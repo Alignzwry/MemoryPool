065bfbf03177cdead36402be07f2c044cdac7f44
Jakob <jakobyl.steffens@gmail.com>
2024-12-24 02:10:55 +0100

    Added Global Pool

--- Memory Pool/main.cpp
+++ Memory Pool/main.cpp
@@ -13,6 +13,17 @@ void* operator new(std::size_t size) {
 #endif
 
 int main() {
+	/// Use Global Pool with String
+	Allocator<char> globalAlloc;
+
+	// Create a std::basic_string that uses that Allocator
+	auto string = std::basic_string<char, std::char_traits<char>, Allocator<char>>(globalAlloc);
+	string = "afakwfjaw";
+
+	for (int i = 0; i < 100; i++)
+		string.append("12334124");
+
+	/// Use Local Pool with String
 	// Create a local instance of StringPool
 	MemoryPool<4096, 32> myPool;
 
@@ -21,13 +32,13 @@ int main() {
 	poolAlloc.Link(myPool);
 
 	// Now create a std::basic_string that uses that Allocator
-	auto string = std::basic_string<char, std::char_traits<char>, Allocator<char, MemoryPool<4096, 32>>>(poolAlloc);
-	string = "afakwfjaw";
+	auto string2 = std::basic_string<char, std::char_traits<char>, Allocator<char, MemoryPool<4096, 32>>>(poolAlloc);
+	string2 = "afakwfjaw";
 
 	for (int i = 0; i < 100; i++)
-		string.append("12334124");
+		string2.append("12334124");
 
-
+	/// Use Local Pool with Vector
 	// Create a pool of 4096 bytes with up to 100 entries
 	MemoryPool<16000, 32> myPool2;
 
